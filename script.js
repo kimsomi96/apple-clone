0e9fece18eb18223942a45f51284d87b4bbbe388
kimsomi96 <kimsomi@pdic.co.kr>
2023-03-14 18:08:58 +0900

    230314

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(){
-
+    const splide = new Splide( '.splide' );
+    splide.mount();
 
     const silver_btn = document.querySelector('.silver_btn');
     const spacegray_btn = document.querySelector('.spacegray_btn');
@@ -7,13 +8,12 @@ document.addEventListener('DOMContentLoaded',function(){
     const sg_img = document.querySelector('.spacegray');
 
     silver_btn.addEventListener('click',function(){
-        s_img.classList.add('on');
-        sg_img.classList.remove('on');
-
+        sg_img.classList.add('on');
+        s_img.classList.remove('on');
     })
     spacegray_btn.addEventListener('click',function(){
 
-        sg_img.classList.add('on');
-        s_img.classList.remove('on');
+        s_img.classList.add('on');
+        sg_img.classList.remove('on');
     })
 });
